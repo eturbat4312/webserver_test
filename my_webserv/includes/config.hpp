e7aec4420293ee519c0e9aa4ba466ca00fa077db
jreivilo <j.reivilo@outlook.com>
2023-06-03 18:05:15 +0200

    makefile + comments

--- my_webserv/includes/config.hpp
+++ my_webserv/includes/config.hpp
@@ -16,28 +16,28 @@ class Locations;
 
 class	errorPages{
 	public:
-		int	status_code;
-		std::string path;
-		std::string page;
+		int						status_code;
+		std::string 			path;
+		std::string 			page;
 };
 
 class Servers{
 public:
-	std::vector<std::string> raw_server;
-	std::string index;
-	std::vector<std::string> server_name;
-	std::vector<int> port;
-	std::string root;
-	std::string returned;
-	bool	isDuplicate;
-	int	_port;
-	int	socket_fd;
-	long	client_body_size;
-	struct sockaddr_in _address;
-	std::string host;
+	std::vector<std::string> 	raw_server;
+	std::string 				index;
+	std::vector<std::string> 	server_name;
+	std::vector<int> 			port;
+	std::string 				root;
+	std::string 				returned;
+	bool						isDuplicate;
+	int							_port;
+	int							socket_fd;
+	long						client_body_size;
+	struct sockaddr_in 			_address;
+	std::string 				host;
 
-	std::vector<Locations> locations;
-	std::vector<errorPages> error_page;
+	std::vector<Locations> 		locations;
+	std::vector<errorPages> 	error_page;
 
 
 	void	split_locations(std::vector<std::string> &server_info);
@@ -59,11 +59,11 @@ public:
 class Locations{
 public:
 	std::vector<std::string> raw_location;
-	std::string root;
-	std::string directive;
-	std::string returned;
-	std::string upload_enable;
-	std::string	upload_store;
+	std::string 			root;
+	std::string 			directive;
+	std::string 			returned;
+	std::string 			upload_enable;
+	std::string				upload_store;
 
 
 };
