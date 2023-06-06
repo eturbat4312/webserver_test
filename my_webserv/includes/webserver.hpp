6f8d60540a0952729f43bd59d52dbc47607d40ba
jreivilo <j.reivilo@outlook.com>
2023-06-06 16:19:06 +0200

    servers + hello world

--- my_webserv/includes/webserver.hpp
+++ my_webserv/includes/webserver.hpp
@@ -29,6 +29,28 @@ public:
 	int address_socket();
 	int	bind_socket();
 	int	listen_socket();
+	int start_server();
 
+	int accept_connection();
+	int read_socket();
+	int write_socket();
+	int close_socket();
+
+	int poll_socket();
+	int poll_loop();
+
+	// int get_max_fd();
+	// int get_fd(int i);
+	// int get_socket_fd(int i);
+	// int get_new_socket(int i);
+	// int get_port(int i);
+	// std::string get_host(int i);
+	// std::string get_root(int i);
+	// std::string get_returned(int i);
+	// std::string get_index(int i);
+	// std::vector<std::string> get_server_name(int i);
+	// std::vector<Locations> get_locations(int i);
+	// std::vector<errorPages> get_error_page(int i);
+	// long get_client_body_size(int i);
 
 };
